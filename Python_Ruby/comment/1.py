d839e6c7c4fbbf3560852bd0c1ad920f203fc035
Aaaaiiiiiee <wjjgjk@naver.com>
2021-05-28 10:57:11 +0000

    Third study of Python&Ruby course. 'comment', 'container'

---
+++ Python_Ruby/comment/1.py
@@ -0,0 +1,11 @@
+input_password = "ab"
+
+real_egoing_password = 11
+#real_k8805_password = "ab"
+
+if real_egoing_password == input_password:
+    print("Hello!, egoing")
+#elif real_k8805_password == input_password:
+#    print("Hello!, k8805")
+else:
+    print("Who are you")
---
+++ Python_Ruby/comment/1.rb
@@ -0,0 +1,18 @@
+=begin
+조건문 예제
+egoing
+2015
+=end
+
+input_password = 33
+
+real_egoing_password = 11
+#real_k8805_password = "ab"
+
+if real_egoing_password == input_password
+    puts("Hello!, egoing")
+#elsif real_k8805_password == input_password
+#        puts("Hello!, k8805")
+else
+    puts("Who are you")
+end
